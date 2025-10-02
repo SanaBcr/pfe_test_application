4819e020278126da24aad9adb897f5d1fdf03c3d
SanaBcr <sana.bkrm@gmail.com>
2025-10-03 00:13:08 +0100

    Update UserRestControlImpl.java

--- src/main/java/tn/esprit/spring/control/UserRestControlImpl.java
+++ src/main/java/tn/esprit/spring/control/UserRestControlImpl.java
@@ -1,27 +1,10 @@
-package tn.esprit.spring.control;
-
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController
-public class UserRestControlImpl {
-
-    // Exemple pédagogique : variable vulnérable volontairement
-    private static final String DEMO_SECRET = "S3cr3tPassw0rd!"; // Sonar devrait le signaler
-
-  @RestController
-public class UserRestControlImpl {
-
-
-
-// http://localhost:8083/SpringMVC/servlet/
-@RequestMapping("/")
-@ResponseBody
-public String welcome() { return "Mise en place d’une plateforme de développement décentralisée sécurisée (mode SaaS) sur un cloud privé."; }
-
- 
-}
- 
+package tn.esprit.spring.control; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.ResponseBody; 
+import org.springframework.web.bind.annotation.RestController; 
+@RestController public class UserRestControlImpl 
+    { // http://localhost:8083/SpringMVC/servlet/ 
+        @RequestMapping("/") 
+        @ResponseBody 
+        public String welcome() { return "Mise en place d’une plateforme de développement décentralisée sécurisée (mode SaaS) sur un cloud privé."; }
+    }
